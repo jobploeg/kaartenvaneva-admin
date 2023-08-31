38622a277829c98ee97eabaead301942c13f6210
job van der ploeg <job.vanderploeg@gmail.com>
2023-08-31 15:45:48 +0200

    fixed error on delete page

--- app/producten/delete/page.js
+++ app/producten/delete/page.js
@@ -1,17 +1,18 @@
 import { supabase } from "../../../lib/supabaseClient";
-import toast from "react-hot-toast";
 
-async function removeProduct(id) {
-  const { data, error } = await supabase.from("products").delete().eq("id", id);
+// async function removeProduct(id) {
+//   const { data, error } = await supabase.from("products").delete().eq("id", id);
 
-  if (data) {
-    toast.success("Product is verwijderd!");
-  }
-  if (error) {
-    toast.error("Er is iets fout gegaan, probeer het later opnieuw!");
-  }
-}
+//   if (data) {
+//     toast.success("Product is verwijderd!");
+//   }
+//   if (error) {
+//     toast.error("Er is iets fout gegaan, probeer het later opnieuw!");
+//   }
+// }
 
 export default async function remove({ params }) {
-  const deleted = await removeProduct(params.id);
+  // const deleted = await removeProduct(params.id);
+
+  return <></>;
 }
